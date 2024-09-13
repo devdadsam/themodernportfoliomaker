865f50e1052df1362af4fe530ccbf183e4e3dda1
Sammy Kordi <Sammy.Kordi@gmail.com>
2024-09-13 14:47:53 -0700

    Added Footer to React

--- themodernpfx/src/App.tsx
+++ themodernpfx/src/App.tsx
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Header';
 import AboutMe from './AboutMe';
 import Portfolio from './Portfolio';
+import Footer from './Footer';
 import Contact from './Contact';
 import Resume from './Resume';
+import './Hero.css'; 
 
 const App: React.FC = () => {
   return (
@@ -16,9 +18,9 @@ const App: React.FC = () => {
         <Route path="/contact" element={<Contact />} />
         <Route path="/resume" element={<Resume />} />
       </Routes>
+      <Footer /> {/* Adding Footer here */}
     </Router>
   );
 };
 
 export default App;
-
